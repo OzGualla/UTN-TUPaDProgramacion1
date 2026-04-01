ce8a0aaec3c4f3bfc3e49c8111ce34c9bbd4bcaf
Mariano.Gualla <guallamariano@gmail.com>
2026-04-01 08:20:11 -0300

    Tp3-listo

---
+++ TrabajoComunitario.py
@@ -0,0 +1,8 @@
+
+""" El original no se modifico """
+
+
+print("\n----------- Espacio reservado para -----------\n"\
+      "--- Trabajo Comunitario, con el Nono Trolo ---\n")
+
+print("Wiwi no tan trolo, pero alto pollera\n")

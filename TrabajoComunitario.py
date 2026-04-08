5baa6135053a878fcddfe1f33a600f6eb4a45ed3
Mariano.Gualla <guallamariano@gmail.com>
2026-04-08 09:36:16 -0300

    se hizo denuevo el tateti

--- TrabajoComunitario.py
+++
@@ -1,8 +0,0 @@
-
-""" El original no se modifico """
-
-
-print("\n----------- Espacio reservado para -----------\n"\
-      "--- Trabajo Comunitario, con el Nono Trolo ---\n")
-
-print("Wiwi no tan trolo, pero alto pollera\n")

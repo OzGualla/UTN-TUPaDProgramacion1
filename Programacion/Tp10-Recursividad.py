5dd6308d11d67aca8eed5f91a60ec53b0cd50d8e
Mariano Gualla <guallamariano@gmail.com>
2026-05-22 22:04:45 -0300

    Incluir Bloques Try/Except

--- Programacion/Tp10-Recursividad.py
+++ Programacion/Tp10-Recursividad.py
@@ -5,6 +5,9 @@
 # Declaración de variables
 # ========================
 
+import sys
+sys.setrecursionlimit(2000)
+
 # Validación de entradas
 def validar_numero(mensaje):
     """
@@ -14,12 +17,12 @@ def validar_numero(mensaje):
     while True:
         num = input(mensaje)
         if not num.isdigit():
-            print("Solo se aceptan numeros positivos")
+            print("Solo se aceptan números positivos")
             continue
 
         num = int(num)
         if num <= 0:
-            print("Error: El numero no puede ser igual o menor a 0")
+            print("Error: El número no puede ser igual o menor a 0")
             continue
 
         return num
@@ -38,6 +41,30 @@ def factorial_recursivo(num):
 # Ejercicio 2:
 # ============
 
+def fibonacci_recursivo(num):
+    # Casos base
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    # En cada iteración F(n) = F(n-1) + F(n-2)
+    else:
+        return fibonacci_recursivo(num - 1) + fibonacci_recursivo(num - 2)
+    
+def ver_serie_fibonacci(num):
+    """
+    Recibe el numero ingresado
+    Recorre y muestra la serie por posición
+    """
+    print(f"\nSerie de Fibonacci hasta la posición {num}\n")
+    for i in range(num + 1):
+        valor = fibonacci_recursivo(i)
+        print(f"Posición: {i} = {valor}")
+
+# ============
+# Ejercicio 3:
+# ============
+
 def potencia_recursiva(n,m):
     if m == 0:
         return 1
@@ -45,17 +72,17 @@ def potencia_recursiva(n,m):
         return n * potencia_recursiva(n,m-1)
 
 # ============
-# Ejercicio 3:
+# Ejercicio 4:
 # ============
 
 def decimal_a_binario(num):
     """ 
     Recibe int(), 
-    Asigna "0","1" segun condicion
+    Asigna "0","1" según condición
     retorna str()
     """
     binario = ""
-    # Caso base, devuelve la variable vacia
+    # Caso base, devuelve la variable vacía
     if num <= 0:
         return binario
     
@@ -70,13 +97,15 @@ def decimal_a_binario(num):
     cociente = num/2
     # Convertir variable a int() para obtener un 0 entero
     cociente = int(cociente)
+
     return decimal_a_binario(cociente)
 
 def menu():
     print("\n=== Menú ===\n" \
     "1 - Factorial\n" \
-    "2 - Exponente\n" \
-    "3 - Conversor binario\n")
+    "2 - Fibonacci\n" \
+    "3 - Exponente\n" \
+    "4 - Convertir a binario\n")
 
 
 # --------------------------------------------------------------------------
@@ -85,6 +114,8 @@ def menu():
 # Código principal
 # ================
 
+# Se utilizan bloques try/except en las llamadas a las funciones
+# para evitar el desbordamiento de pila en caso de recursión excesiva.
 while True:
 
     menu()
@@ -99,11 +130,28 @@ while True:
             print("=== Factorial ===")
             numero = validar_numero("Ingrese un numero, para ver su factorial: ")
 
-            print(f"El factorial de {numero} es: {factorial_recursivo(numero)}")
+            try:
+                print(f"El factorial de {numero} es: {factorial_recursivo(numero)}")
 
+            except RecursionError:
+                print(f"\nError: Se ha sobrepasado el limite de pila")
+        
         case 2:
             # ============
             # Ejercicio 2:
+            # ============
+            numero = validar_numero("Ingrese un numero para ver la secuencia Fibonacci: ")
+
+            try:
+                print(f"\nValor Fibonacci en la posición {numero}: {fibonacci_recursivo(numero)}")
+                ver_serie_fibonacci(numero)
+
+            except RecursionError:
+                print(f"\nError: Se ha sobrepasado el limite de pila")
+
+        case 3:
+            # ============
+            # Ejercicio 3:
             # ============
                 
             print("\n=== Exponente ===")
@@ -111,19 +159,38 @@ while True:
 
             exponente = validar_numero("Ingrese exponente: ")
 
-            numero_potenciado = potencia_recursiva(numero,exponente)
+            numero_potenciado = ""
 
-            print(f"\n{numero} elevado a {exponente} es: {numero_potenciado}\n")
+            try:
+                numero_potenciado = potencia_recursiva(numero,exponente)
+                print(f"\n{numero} elevado a {exponente} es: {numero_potenciado}\n")
 
-        case 3:
+            except RecursionError:
+                print(f"\nError: Se ha sobrepasado el limite de pila")
+
+            # Prueba en algoritmo General:
+            lista_numero_y_exponente = [
+                (2,3),(3,2),(4,4),(6,2),(8,3),(10,4)
+            ]
+            print("\n=== Prueba de diferentes valores potenciados ===")
+            # Se le pasa una lista de valores para probar diferentes combinaciones
+            for numero,exponente in lista_numero_y_exponente:
+                print(f"\n{numero} elevado a {exponente} es: {potencia_recursiva(numero,exponente)}")
+
+        case 4:
             # ============
-            # Ejercicio 3:
+            # Ejercicio 4:
             # ============
 
             print("=== Convertir a número binario ===")
 
             numero_a_binario = validar_numero("Ingrese número: ")
-            decimal_a_binario(numero_a_binario)
+
+            try:
+                decimal_a_binario(numero_a_binario)
+
+            except RecursionError:
+                print(f"\nError: Se ha sobrepasado el limite de pila")
         
         case _:
-            print("=== Opcion invalida ===")
+            print("=== Opción invalida ===")
